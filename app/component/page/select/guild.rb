2e26cad83dce3b14864002c4bdcef68c9ab1e70a
meh <meh@schizofreni.co>
2014-01-05 21:17:12 +0100

    page/select/guild: make it work

--- app/component/page/select/guild.rb
+++ app/component/page/select/guild.rb
@@ -1,6 +1,14 @@
 module Component; class Page < Lissio::Component::Container; module Select
 
 class Guild < Lissio::Component
+	on :keydown, 'input' do |e|
+		next unless e.key == :Enter
+
+		unless e.target.value.empty?
+			Application.navigate("/guild/#{e.target.value.encode_uri_component}")
+		end
+	end
+
 	tag id: 'select-guild'
 
 	html do
@@ -11,9 +19,16 @@ class Guild < Lissio::Component
 
 	css do
 		rule '#select-guild' do
+			text align: :center
+
 			rule 'input' do
 				background :white
-				border 1.px, :solid, :black
+				border :none
+				border bottom: [1.px, :dashed, :black]
+
+				font size: 23.px
+				width 80.%
+				color :black
 			end
 		end
 	end
