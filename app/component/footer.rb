f389725de2133da080c627b40bde5363d9c22191
meh <meh@schizofreni.co>
2014-01-06 20:23:05 +0100

    footer: add a menu to the footer

--- app/component/footer.rb
+++ app/component/footer.rb
@@ -18,6 +18,27 @@
 module Component
 
 class Footer < Lissio::Component
+	on :click, 'a', :hide
+	on :click, '.arrow > div', :toggle
+
+	def show
+		element.style.height = 205.px
+		element.at_css('.arrow div').remove_class(:up).add_class(:down)
+	end
+
+	def hide
+		element.style.height = 150.px
+		element.at_css('.arrow div').remove_class(:down).add_class(:up)
+	end
+
+	def toggle
+		if element.style.height.to_u == 150.px
+			show
+		else
+			hide
+		end
+	end
+
 	element '#footer'
 
 	on :render do
@@ -33,6 +54,15 @@ class Footer < Lissio::Component
 			span.name "GWENTOO"
 			span.version Gwentoo::VERSION.to_s
 		end
+
+		div.arrow do
+			div.down
+		end
+
+		div.menu do
+			a.href('/').text('Home')
+			a.href('/select/guild').text('Guild')
+		end
 	end
 
 	css do
@@ -41,8 +71,8 @@ class Footer < Lissio::Component
 			bottom   0
 			left     0
 
-			width    100.%
-			height 150.px
+			width  100.%
+			height 205.px
 
 			background url('img/footer.jpg'), 50.%, 0.%, 'no-repeat'
 
@@ -59,8 +89,11 @@ class Footer < Lissio::Component
 
 			rule '.gwentoo' do
 				position :absolute
-				right 15.%
-				top 90.px
+				left     50.%
+				top      90.px
+
+				margin left: 210.px
+
 				color '#bbb'
 
 				rule '.name' do
@@ -75,9 +108,45 @@ class Footer < Lissio::Component
 					margin left: 7.px
 				end
 			end
+
+			rule '.arrow' do
+				width  100.%
+				margin top: 75.px
+
+				text align: :center
+
+				rule '& > div' do
+					width  13.px
+					height 8.px
+
+					display 'inline-block'
+
+					rule '&.up' do
+						background url('img/arrow.up.png')
+					end
+
+					rule '&.down' do
+						background url('img/arrow.down.png')
+					end
+				end
+			end
+
+			rule '.menu' do
+				position :absolute
+				top      165.px
+				left     0
+
+				width 100.%
+				text align: :center
+
+				font size: 23.px
+
+				rule 'a' do
+					margin 0, 10.px
+				end
+			end
 		end
 	end
-
 end
 
 end
